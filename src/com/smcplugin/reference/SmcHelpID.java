538ea231828938fbbd2e02838e0aa65feaf251ae
Leonid Menshenin <leonid.menshenin@gmail.com>
2016-03-25 23:31:56 +0300

    smcplugin-4 Implement Code validation and Linking

---
+++ src/com/smcplugin/reference/SmcHelpID.java
@@ -0,0 +1,14 @@
+package com.smcplugin.reference;
+
+/**
+ * scmplugin
+ * Created by lemen on 25.03.2016.
+ */
+
+import org.jetbrains.annotations.NonNls;
+
+public interface SmcHelpID {
+    @NonNls String FIND_MAP_USAGES = "reference.dialogs.findUsages.map";
+    @NonNls String FIND_STATE_USAGES = "reference.dialogs.findUsages.state";
+    @NonNls String FIND_ACTION_USAGES = "reference.dialogs.findUsages.action";
+}
---
+++ src/com/smcplugin/reference/SmcUsagesProvider.java
@@ -0,0 +1,76 @@
+package com.smcplugin.reference;
+
+import com.intellij.lang.cacheBuilder.WordsScanner;
+import com.intellij.lang.findUsages.FindUsagesProvider;
+import com.intellij.psi.PsiElement;
+import com.smcplugin.psi.SmcAction;
+import com.smcplugin.psi.SmcMap;
+import com.smcplugin.psi.SmcNamedElement;
+import com.smcplugin.psi.SmcState;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * scmplugin
+ * Created by lemen on 25.03.2016.
+ */
+public class SmcUsagesProvider implements FindUsagesProvider {
+    @Nullable
+    @Override
+    public WordsScanner getWordsScanner() {
+        return null;
+    }
+
+    @Override
+    public boolean canFindUsagesFor(@NotNull PsiElement element) {
+        return element instanceof SmcMap ||
+                element instanceof SmcState ||
+                element instanceof SmcAction;
+    }
+
+    @Override
+    public String getHelpId(@NotNull PsiElement element) {
+        if (element instanceof SmcMap) {
+            return SmcHelpID.FIND_MAP_USAGES;
+        }
+        if (element instanceof SmcState) {
+            return SmcHelpID.FIND_STATE_USAGES;
+        }
+        if (element instanceof SmcAction) {
+            return SmcHelpID.FIND_ACTION_USAGES;
+        }
+        return com.intellij.lang.HelpID.FIND_OTHER_USAGES;
+    }
+
+    @NotNull
+    @Override
+    public String getType(@NotNull PsiElement element) {
+        if (element instanceof SmcMap) {
+            return "Smc map";
+        } else if (element instanceof SmcState){
+            return "Smc state";
+        } else if (element instanceof SmcAction){
+            return "Smc action";
+        }else{
+            return "";
+        }
+    }
+
+    @NotNull
+    @Override
+    public String getDescriptiveName(@NotNull PsiElement element) {
+        if (element instanceof SmcNamedElement) {
+            SmcNamedElement smcNamedElement = (SmcNamedElement) element;
+            String name = smcNamedElement.getName();
+            return name != null ? name : "";
+        } else {
+            return "";
+        }
+    }
+
+    @NotNull
+    @Override
+    public String getNodeText(@NotNull PsiElement element, boolean useFullName) {
+        return getDescriptiveName(element);
+    }
+}
